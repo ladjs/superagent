c3d53ee0e8ada58e3495cf6c998455176336c3bf
Tj Holowaychuk <tj@vision-media.ca>
2011-04-12 16:54:39 -0700

    started basic implementation

--- test/cases/basic.js
+++ test/cases/basic.js
@@ -1,5 +1,23 @@
 
 require('./../common');
 
-var just = require('just');
+var just = require('just')
+  , express = require('express')
+  , app = express.createServer();
+
 just.should.have.property('version');
+
+app.get('/', function(req, res){
+  res.end('Hello');
+});
+
+app.listen(3000);
+
+var req = just.request('GET', 'http://127.0.0.1:3000');
+
+req.on('response', function(res){
+  res.statusCode.should.equal(200);
+  app.close();
+});
+
+req.end();
