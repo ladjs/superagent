b6f259d8af41518c8c885ebb853409f4d83a86c6
Tj Holowaychuk <tj@vision-media.ca>
2011-04-17 18:51:54 -0700

    added json message support

---
+++ test/cases/basic.post.json.js
@@ -0,0 +1,24 @@
+
+require('./../common');
+
+var agent = require('superagent')
+  , express = require('express')
+  , app = express.createServer();
+
+app.post('/', function(req, res){
+  var buf = '';
+  req.headers.should.have.property('content-type', 'application/json');
+  req.on('data', function(chunk){ buf += chunk; });
+  req.on('end', function(){
+    buf.should.equal('{"foo":"bar"}');
+    app.close();
+  });
+  res.end();
+});
+
+app.listen(3000, function(){
+  agent
+    .post('http://localhost:3000')
+    .json({ foo: 'bar' })
+    .end();
+});
