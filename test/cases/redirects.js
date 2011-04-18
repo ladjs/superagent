6a59b08e4cf107dd15027db8e79ee788e86e6738
Tj Holowaychuk <tj@vision-media.ca>
2011-04-17 18:03:54 -0700

    more testing

--- test/cases/redirects.js
+++ test/cases/redirects.js
@@ -5,6 +5,8 @@ var agent = require('superagent')
   , express = require('express')
   , app = express.createServer();
 
+var redirects = [];
+
 agent.should.have.property('version');
 
 app.get('/', function(req, res){
@@ -20,13 +22,21 @@ app.get('/two', function(req, res){
 });
 
 app.listen(3000, function(){
-  agent.request('GET', 'http://localhost:3000')
+  var req = agent.request('GET', 'http://localhost:3000')
     .buffer()
     .on('response', function(res){
       res.statusCode.should.equal(200);
+
       res.on('end', function(){
         res.body.should.equal('done');
+        redirects.should.eql(['http://localhost:3000/one', 'http://localhost:3000/two']);
         app.close();
       });
-    }).end();
+    });
+
+  req.on('redirect', function(location){
+    redirects.push(location);
+  });
+  
+  req.end();
 });
