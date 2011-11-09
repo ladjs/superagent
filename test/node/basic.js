094b4b517989c0586c863c4eb82945ad94f268ab
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 21:13:52 -0800

    Added fire-and-forget .end() test

--- test/node/basic.js
+++ test/node/basic.js
@@ -9,12 +9,26 @@ app.get('/login', function(req, res){
 
 app.listen(3000);
 
-describe('request(method, url).end(fn)', function(){
-  it('should issue a request', function(done){
-    request('GET', 'http://localhost:3000/login')
-    .end(function(res){
-      res.status.should.equal(200);
-      done();
-    });
+describe('request(method, url)', function(){
+  describe('.end()', function(){
+    it('should issue a request', function(done){
+      request('GET', 'http://localhost:3000/login')
+      .on('response', function(res){
+        // TODO: make this a superagent.Response
+        res.statusCode.should.equal(200);
+        done();
+      })
+      .end();
+    })
+  })
+
+  describe('.end(fn)', function(){
+    it('should issue a request', function(done){
+      request('GET', 'http://localhost:3000/login')
+      .end(function(res){
+        res.status.should.equal(200);
+        done();
+      });
+    })
   })
 })
