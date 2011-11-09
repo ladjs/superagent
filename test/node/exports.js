09965a115d18c008f934441a76188127e8c0534c
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 21:01:51 -0800

    Added more export tests

--- test/node/exports.js
+++ test/node/exports.js
@@ -23,4 +23,14 @@ describe('exports', function(){
     Object.keys(request.types)
       .should.eql(['html', 'json', 'urlencoded', 'form-data']);
   })
+  
+  it('should expose .serialize', function(){
+    Object.keys(request.serialize)
+      .should.eql(['application/x-www-form-urlencoded', 'application/json']);
+  })
+  
+  it('should expose .parse', function(){
+    Object.keys(request.parse)
+      .should.eql(['application/x-www-form-urlencoded', 'application/json']);
+  })
 })
