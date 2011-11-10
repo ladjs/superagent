588f80c832deea1ab3d9931d2a364d8a473e55ac
Tj Holowaychuk <tj@vision-media.ca>
2011-11-09 21:13:16 -0800

    more multipart tests

--- test/node/multipart.js
+++ test/node/multipart.js
@@ -41,6 +41,46 @@ describe('Request', function(){
       })
     })
     
+    describe('with several parts', function(){
+      it('should construct a multipart request', function(done){
+
+        var req = request.post('http://localhost:3005/echo');
+
+        req.part()
+          .set('Content-Type', 'image/png')
+          .set('Content-Disposition', 'attachment')
+          .write('some image data');
+            
+        var part = req.part()
+          .set('Content-Type', 'text/plain');
+
+        part.write('foo ');
+        part.write('bar ');
+        part.write('baz');
+
+        req.end(function(res){
+          var ct = res.header['content-type'];
+          ct.should.include.string('multipart/mixed; boundary="');
+          var boundary = ct.match(/boundary="(.*)"/)[1];
+
+          var body = '';
+          body += '\r\n--' + boundary + '\r\n';
+          body += 'Content-Type: image/png\r\n';
+          body += 'Content-Disposition: attachment\r\n';
+          body += '\r\n';
+          body += 'some image data';
+          body += '\r\n--' + boundary + '\r\n';
+          body += 'Content-Type: text/plain\r\n';
+          body += '\r\n';
+          body += 'foo bar baz';
+          body += '\r\n--' + boundary + '--';
+
+          assert(body == res.text, 'invalid multipart response');
+          done();
+        });
+      })
+    })
+    
     describe('with a Content-Type specified', function(){
       it('should append the boundary', function(){
         var req = request.post('http://localhost:3005/echo');
