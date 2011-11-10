35d18675c6f56860995a74769871f8dd2cfa4e6f
Tj Holowaychuk <tj@vision-media.ca>
2011-11-09 17:23:24 -0800

    more tests

--- test/node/basic.js
+++ test/node/basic.js
@@ -1,32 +1,58 @@
 
 var request = require('../../')
   , express = require('express')
+  , assert = require('assert')
   , app = express.createServer();
 
 app.get('/login', function(req, res){
   res.send('<form id="login"></form>');
 });
 
+app.get('/error', function(req, res){
+  throw new Error('oh noes');
+});
+
 app.listen(3000);
 
-describe('request(method, url)', function(){
-  describe('.end()', function(){
-    it('should issue a request', function(done){
-      request('GET', 'http://localhost:3000/login')
-      .on('response', function(res){
-        // TODO: make this a superagent.Response
-        res.statusCode.should.equal(200);
+// TODO: "response" event should be a Response
+
+describe('request.VERB(path)', function(){
+  describe('with 4xx response', function(){
+    it('should set flags properly', function(done){
+      request
+      .get('http://localhost:3000/notfound')
+      .end(function(res){
+        assert(!res.ok, 'response should not be ok');
+        assert(res.notFound, 'response should be notFound');
+        assert(res.error, 'response should be an error');
+        assert(res.clientError, 'response should be a client error');
+        assert(!res.serverError, 'response should not be a server error');
         done();
-      })
-      .end();
+      });
+    })
+  })
+  
+  describe('with 5xx response', function(){
+    it('should set flags properly', function(done){
+      request
+      .get('http://localhost:3000/error')
+      .end(function(res){
+        assert(!res.ok, 'response should not be ok');
+        assert(!res.notFound, 'response should not be notFound');
+        assert(res.error, 'response should be an error');
+        assert(!res.clientError, 'response should not be a client error');
+        assert(res.serverError, 'response should be a server error');
+        done();
+      });
     })
   })
 
-  describe('.end(fn)', function(){
+  describe('.end()', function(){
     it('should issue a request', function(done){
-      request('GET', 'http://localhost:3000/login')
+      request
+      .get('http://localhost:3000/login')
       .end(function(res){
-        res.status.should.equal(200);
+        assert(res.status == 200);
         done();
       });
     })
