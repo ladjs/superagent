a629ed59a7c4c4c644e14f1a312a3dc524fd6693
Tj Holowaychuk <tj@vision-media.ca>
2011-11-09 21:45:13 -0800

    Added GET .data() test

---
+++ test/node/query.js
@@ -0,0 +1,29 @@
+
+var request = require('../../')
+  , express = require('express')
+  , assert = require('assert')
+  , app = express.createServer();
+
+app.get('/', function(req, res){
+  res.send(req.query);
+});
+
+app.listen(3006);
+
+// TODO: "response" event should be a Response
+
+describe('req.data(Object)', function(){
+  describe('on a GET request', function(){
+    it('should send x-www-form-urlencoded data', function(done){
+      request
+      .get('http://localhost:3006/')
+      .data({ name: 'tobi' })
+      .data({ order: 'asc' })
+      .data({ limit: ['1', '2'] })
+      .end(function(res){
+        res.body.should.eql({ name: 'tobi', order: 'asc', limit: ['1', '2'] });
+        done();
+      });
+    })
+  })
+})
