d822dcd73668ad50925ecde343a1f531d908d3c3
TJ Holowaychuk <tj@vision-media.ca>
2011-12-22 05:23:44 -0800

    refactoring tests

--- test/node/flags.js
+++ test/node/flags.js
@@ -31,86 +31,89 @@ app.get('/no-content', function(req, res){
 
 app.listen(3004);
 
-describe('with 4xx response', function(){
-  it('should set res.error and res.clientError', function(done){
-    request
-    .get('http://localhost:3004/notfound')
-    .end(function(res){
-      assert(!res.ok, 'response should not be ok');
-      assert(res.error, 'response should be an error');
-      assert(res.clientError, 'response should be a client error');
-      assert(!res.serverError, 'response should not be a server error');
-      done();
-    });
+describe('flags', function(){
+  
+  describe('with 4xx response', function(){
+    it('should set res.error and res.clientError', function(done){
+      request
+      .get('http://localhost:3004/notfound')
+      .end(function(res){
+        assert(!res.ok, 'response should not be ok');
+        assert(res.error, 'response should be an error');
+        assert(res.clientError, 'response should be a client error');
+        assert(!res.serverError, 'response should not be a server error');
+        done();
+      });
+    })
   })
-})
 
-describe('with 5xx response', function(){
-  it('should set res.error and res.serverError', function(done){
-    request
-    .get('http://localhost:3004/error')
-    .end(function(res){
-      assert(!res.ok, 'response should not be ok');
-      assert(!res.notFound, 'response should not be notFound');
-      assert(res.error, 'response should be an error');
-      assert(!res.clientError, 'response should not be a client error');
-      assert(res.serverError, 'response should be a server error');
-      done();
-    });
+  describe('with 5xx response', function(){
+    it('should set res.error and res.serverError', function(done){
+      request
+      .get('http://localhost:3004/error')
+      .end(function(res){
+        assert(!res.ok, 'response should not be ok');
+        assert(!res.notFound, 'response should not be notFound');
+        assert(res.error, 'response should be an error');
+        assert(!res.clientError, 'response should not be a client error');
+        assert(res.serverError, 'response should be a server error');
+        done();
+      });
+    })
   })
-})
 
-describe('with 404 Not Found', function(){
-  it('should res.notFound', function(done){
-    request
-    .get('http://localhost:3004/notfound')
-    .end(function(res){
-      assert(res.notFound, 'response should be .notFound');
-      done();
-    });
+  describe('with 404 Not Found', function(){
+    it('should res.notFound', function(done){
+      request
+      .get('http://localhost:3004/notfound')
+      .end(function(res){
+        assert(res.notFound, 'response should be .notFound');
+        done();
+      });
+    })
   })
-})
 
-describe('with 400 Bad Request', function(){
-  it('should set req.badRequest', function(done){
-    request
-    .get('http://localhost:3004/bad-request')
-    .end(function(res){
-      assert(res.badRequest, 'response should be .badRequest');
-      done();
-    });
+  describe('with 400 Bad Request', function(){
+    it('should set req.badRequest', function(done){
+      request
+      .get('http://localhost:3004/bad-request')
+      .end(function(res){
+        assert(res.badRequest, 'response should be .badRequest');
+        done();
+      });
+    })
   })
-})
 
-describe('with 401 Bad Request', function(){
-  it('should set res.unauthorized', function(done){
-    request
-    .get('http://localhost:3004/unauthorized')
-    .end(function(res){
-      assert(res.unauthorized, 'response should be .unauthorized');
-      done();
-    });
+  describe('with 401 Bad Request', function(){
+    it('should set res.unauthorized', function(done){
+      request
+      .get('http://localhost:3004/unauthorized')
+      .end(function(res){
+        assert(res.unauthorized, 'response should be .unauthorized');
+        done();
+      });
+    })
   })
-})
 
-describe('with 406 Not Acceptable', function(){
-  it('should set res.notAcceptable', function(done){
-    request
-    .get('http://localhost:3004/not-acceptable')
-    .end(function(res){
-      assert(res.notAcceptable, 'response should be .notAcceptable');
-      done();
-    });
+  describe('with 406 Not Acceptable', function(){
+    it('should set res.notAcceptable', function(done){
+      request
+      .get('http://localhost:3004/not-acceptable')
+      .end(function(res){
+        assert(res.notAcceptable, 'response should be .notAcceptable');
+        done();
+      });
+    })
   })
-})
 
-describe('with 204 No Content', function(){
-  it('should set res.noContent', function(done){
-    request
-    .get('http://localhost:3004/no-content')
-    .end(function(res){
-      assert(res.noContent, 'response should be .noContent');
-      done();
-    });
+  describe('with 204 No Content', function(){
+    it('should set res.noContent', function(done){
+      request
+      .get('http://localhost:3004/no-content')
+      .end(function(res){
+        assert(res.noContent, 'response should be .noContent');
+        done();
+      });
+    })
   })
 })
