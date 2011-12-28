869af3be5a77932d5e99ccddc46bbcf594aab1bd
TJ Holowaychuk <tj@vision-media.ca>
2011-12-27 17:09:33 -0700

    tweak tests

--- test/node/basic-auth.js
+++ test/node/basic-auth.js
@@ -14,27 +14,13 @@ app.get('/', function(req, res){
 app.listen(3010);
 
 describe('req.auth(user, pass)', function(){
-  describe('when valid', function(){
-    it('should serve the request', function(done){
-      request
-      .get('http://localhost:3010')
-      .auth('tobi', 'learnboost')
-      .end(function(res){
-        res.status.should.equal(200);
-        done();
-      });
-    })
-  })
-
-  describe('when invalid', function(){
-    it('should 401', function(done){
-      request
-      .get('http://localhost:3010')
-      .auth('tobis', 'learnboost')
-      .end(function(res){
-        res.status.should.equal(401);
-        done();
-      });
-    })
+  it('should set Authorization', function(done){
+    request
+    .get('http://localhost:3010')
+    .auth('tobi', 'learnboost')
+    .end(function(res){
+      res.status.should.equal(200);
+      done();
+    });
   })
 })
