fbb1930fcbb32775ccf85701ae992238a8774859
TJ Holowaychuk <tj@vision-media.ca>
2012-01-13 19:43:28 -0800

    Added test for utils.uid()

--- test/node/utils.js
+++ test/node/utils.js
@@ -1,6 +1,14 @@
 
 var utils = require('../../lib/node/utils');
 
+describe('utils.uid(len)', function(){
+  it('should generate a unique id', function(){
+    utils.uid(10).should.have.length(10);
+    utils.uid(30).should.have.length(30);
+    utils.uid(30).should.not.equal(utils.uid(30));
+  })
+})
+
 describe('utils.type(obj)', function(){
   it('should return the mime type', function(){
     var res = { headers: { 'content-type': 'application/json; charset=utf-8' }};
