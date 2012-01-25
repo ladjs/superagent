dbb0f03af07b47b4469ab6a9f0c68a0d63c91f14
Damon Oehlman <damon.oehlman@sidelab.com>
2012-01-25 13:17:19 +1000

    Updated querystring tests to run tests against both GET and DELETE methods

--- test/node/query.js
+++ test/node/query.js
@@ -8,41 +8,47 @@ app.get('/', function(req, res){
   res.send(req.query);
 });
 
+app.del('/', function(req, res) {
+    res.send(req.query);
+});
+
 app.listen(3006);
 
 // TODO: "response" event should be a Response
 
 describe('req.send(Object)', function(){
-  describe('on a GET request', function(){
-    it('should construct the query-string', function(done){
-      request
-      .get('http://localhost:3006/')
-      .send({ name: 'tobi' })
-      .send({ order: 'asc' })
-      .send({ limit: ['1', '2'] })
-      .end(function(res){
-        res.body.should.eql({ name: 'tobi', order: 'asc', limit: ['1', '2'] });
-        done();
-      });
-    })
+  ['get', 'del'].forEach(function(method) {
+      describe('on a ' + method + ' request', function(){
+        it('should construct the query-string', function(done){
+          request
+          [method]('http://localhost:3006/')
+          .send({ name: 'tobi' })
+          .send({ order: 'asc' })
+          .send({ limit: ['1', '2'] })
+          .end(function(res){
+            res.body.should.eql({ name: 'tobi', order: 'asc', limit: ['1', '2'] });
+            done();
+          });
+        })
 
-    it('should append to the original query-string', function(done){
-      request
-      .get('http://localhost:3006/?name=tobi')
-      .send({ order: 'asc' })
-      .end(function(res) {
-        res.body.should.eql({ name: 'tobi', order: 'asc' });
-        done();
-      });
-    });
+        it('should append to the original query-string', function(done){
+          request
+          [method]('http://localhost:3006/?name=tobi')
+          .send({ order: 'asc' })
+          .end(function(res) {
+            res.body.should.eql({ name: 'tobi', order: 'asc' });
+            done();
+          });
+        });
 
-    it('should retain the original query-string', function(done){
-      request
-      .get('http://localhost:3006/?name=tobi')
-      .end(function(res) {
-        res.body.should.eql({ name: 'tobi' });
-        done();
-      });
-    });
-  })
+        it('should retain the original query-string', function(done){
+          request
+          [method]('http://localhost:3006/?name=tobi')
+          .end(function(res) {
+            res.body.should.eql({ name: 'tobi' });
+            done();
+          });
+        });
+      })      
+  });
 })
