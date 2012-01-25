e1799c39405a6c3392d2fe10b2522cfc9b009459
Tj Holowaychuk <tj@vision-media.ca>
2012-01-24 21:59:53 -0800

    Added "response" event providing a Response object. Closes #28
    this has the side-effect of providing a nicer stream api

---
+++ test/node/pipe.js
@@ -0,0 +1,31 @@
+
+var request = require('../../')
+  , express = require('express')
+  , app = express.createServer()
+  , fs = require('fs');
+
+app.use(express.bodyParser());
+
+app.post('/', function(req, res){
+  res.send(req.body);
+});
+
+app.listen(3020);
+
+describe('request', function(){
+  describe('act as a writable stream', function(){
+    it('should format the url', function(done){
+      var req = request.post('http://localhost:3020')
+        , stream = fs.createReadStream('test/node/fixtures/user.json');
+
+      req.type('json');
+
+      req.on('response', function(res){
+        res.body.should.eql({ name: 'tobi' });
+        done();
+      });
+
+      stream.pipe(req);
+    })
+  })
+})
