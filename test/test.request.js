0c529539503958ebc7939c12903d57bad20e5b5e
TJ Holowaychuk <tj@vision-media.ca>
2012-07-17 14:11:18 -0700

    fix typo

--- test/test.request.js
+++ test/test.request.js
@@ -296,7 +296,7 @@ test('POST multiple .send() strings', function(next){
   .end(function(res){
     assert(res.header['content-type'] == 'application/x-www-form-urlencoded');
     assert(res.text == 'user[name]=tj&user[email]=tj@vision-media.ca')
-    done();
+    next();
   })
 });
 
