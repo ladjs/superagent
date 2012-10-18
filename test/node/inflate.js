9baf38bf43139e724b20eec01d65ccf3721527d9
TJ Holowaychuk <tj@vision-media.ca>
2012-10-17 18:02:53 -0700

    fix emission of "data" events for compressed responses without a specified encoding. Closes #125

--- test/node/inflate.js
+++ test/node/inflate.js
@@ -17,6 +17,13 @@ if (zlib) {
 
   app.listen(3080);
 
+  app.get('/binary', function(req, res){
+    zlib.deflate(subject, function (err, buf){
+      res.set('Content-Encoding', 'gzip');
+      res.send(buf);
+    });
+  });
+
   app.get('/', function (req, res, next){
     zlib.deflate(subject, function (err, buf){
       res.set('Content-Type', 'text/plain');
@@ -36,5 +43,22 @@ if (zlib) {
           done();
         });
     });
+
+    describe('without encoding set', function(){
+      it('should emit buffers', function(done){
+        request
+          .get('http://localhost:3080/binary')
+          .end(function(res){
+            res.should.have.status(200);
+            res.headers['content-length'].should.be.below(subject.length);
+
+            res.on('data', function(chunk){
+              chunk.should.have.length(subject.length);
+            });
+
+            res.on('end', done);
+          });
+      })
+    })
   });
 }
