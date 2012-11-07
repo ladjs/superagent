515f21d8168f131706dbda29b639cd8fa80db1b0
Eric Edem <ericedem@gmail.com>
2012-11-06 17:23:20 -0800

    made formatting changes for pull request.

--- test/node/parse.js
+++ test/node/parse.js
@@ -1,25 +1,28 @@
 
 var EventEmitter = require('events').EventEmitter
-    , request = require('../../')
-    , express = require('express')
-    , assert = require('assert')
-    , app = express();
+  , request = require('../../')
+  , express = require('express')
+  , assert = require('assert')
+  , app = express();
 
 app.get('/stuff', function(req, res){
-    res.send('{"name":"manny"}');
+  res.send('{"name":"manny"}');
 })
 app.listen(3025);
 
-describe('weird/mimetype', function(){
+describe('.parse(fn)', function(){
+  describe('weird/mimetype',function(){
     it('should parse the body', function(done){
-        request
-            .get('http://localhost:3025/stuff')
-            .parse(request.parse['application/json']) // just use the default json parser
-            .end(function(res){
-                // should not default to json content type
-                res.text.should.equal('{"name":"manny"}');
-                res.body.should.eql({ name: 'manny' });
-                done();
-            });
+      request
+      .get('http://localhost:3025/stuff')
+      .parse(request.parse['application/json']) // just use the default json parser
+      .end(function(res){
+        // should not default to json content type
+        res.text.should.equal('{"name":"manny"}');
+        res.body.should.eql({ name: 'manny' });
+        done();
+      });
     })
+  });
+
 })
