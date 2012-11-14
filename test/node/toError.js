f25347621b19c7712633a2fae95e2deae09dba55
TJ Holowaychuk <tj@vision-media.ca>
2012-11-14 14:22:09 -0800

    add toError tests

---
+++ test/node/toError.js
@@ -0,0 +1,25 @@
+
+var request = require('../..')
+  , express = require('express')
+  , assert = require('better-assert')
+  , app = express()
+  , url = require('url');
+
+app.get('/', function(req, res){
+  res.send(400, 'invalid json');
+});
+
+app.listen(8888);
+
+describe('res.toError()', function(){
+  it('should return an Error', function(done){
+    request
+    .get('http://localhost:8888/')
+    .end(function(res){
+      var err = res.toError();
+      assert(err.status == 400);
+      assert(err.message == 'got 400 response');
+      done();
+    });
+  })
+})
