f90d83f421d5c7fbcb35307d23223ba9949feb4c
jkroso <jkroso@gmail.com>
2013-05-06 00:55:07 +1200

    fix readable stream test

--- test/node/response-readable-stream.js
+++ test/node/response-readable-stream.js
@@ -11,29 +11,18 @@ app.get('/', function(req, res){
 app.listen(3025);
 
 describe('response', function(){
-    it('should act as a readable stream', function(done){
-      var req = request
-        .get('http://localhost:3025')
-        .buffer(false);
+  it('should act as a readable stream', function(done){
+    var req = request
+      .get('http://localhost:3025')
+      .buffer(false);
 
-      req.end(function(err,res){
-        if (err) return done(err);
-        var trackEndEvent = 0;
-        var trackCloseEvent = 0;
-        res.on('end',function(){
-          trackEndEvent++;
-        });
+    req.end(function(err,res){
+      if (err) return done(err);
+      res.on('end', done);
 
-        res.on('close',function(){
-          trackCloseEvent++;
-          trackEndEvent.should.equal(1);
-          trackCloseEvent.should.equal(1);
-          done();
-        });
-
-        (function(){ res.pause() }).should.not.throw();
-        (function(){ res.resume() }).should.not.throw();
-        (function(){ res.destroy() }).should.not.throw();
-      });
+      (function(){ res.pause() }).should.not.throw();
+      (function(){ res.resume() }).should.not.throw();
+      (function(){ res.destroy() }).should.not.throw();
     });
+  });
 });
