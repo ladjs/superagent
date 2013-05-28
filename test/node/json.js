c36e1f746a9c126c55ff514567e8a748e4dde2b5
Remy Loubradou <remyloubradou@gmail.com>
2013-05-28 13:55:30 +0100

    Add failing test using pipe style when an error is thrown by JSON.stringify.

--- test/node/json.js
+++ test/node/json.js
@@ -28,7 +28,7 @@ describe('req.send(Object) as "json"', function(){
     });
   })
 
-  it('should catch error when serializing',function(done){
+  it('should catch error when serializing (callback style)',function(done){
     function CircularReference(){
       this.circular = this;
     }
@@ -40,6 +40,25 @@ describe('req.send(Object) as "json"', function(){
       err.should.be.an.instanceOf(Error);
       done();
     });
+  });
+
+  it('should catch error when serializing (pipe style)',function(done){
+    function CircularReference(){
+      this.circular = this;
+    }
+    var s = new Stream();
+    s.writable = true;
+    s.write = function(data){
+      // don't care
+    };
+    request
+    .post('http://localhost:3005/echo')
+    .send(new CircularReference())
+    .on('error',function(err){
+      err.should.be.an.instanceOf(Error);
+      done();
+    })
+    .pipe(s);
   });
 
   it('should work with arrays', function(done){
