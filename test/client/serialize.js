1859a620ea2daa8388d120d37ae518ed81fe5399
alekzonder <alekzonder@gmail.com>
2015-03-21 14:06:52 +0600

    add tests for serialize, parse for nested objects, fix some asserts

--- test/client/serialize.js
+++ test/client/serialize.js
@@ -23,13 +23,15 @@ describe('request.serializeObject()', function(){
     serialize('test', 'test');
     serialize('foo=bar', 'foo=bar');
     serialize({ foo: 'bar' }, 'foo=bar');
-    serialize({ foo: null }, '');
+    serialize({ foo: null }, 'foo=');
     serialize({ foo: 'null' }, 'foo=null');
     serialize({ foo: undefined }, '');
     serialize({ foo: 'undefined' }, 'foo=undefined');
     serialize({ name: 'tj', age: 24 }, 'name=tj&age=24');
-      serialize({ name: '&tj&' }, 'name=%26tj%26');
-      serialize({ '&name&': 'tj' }, '%26name%26=tj');
+    serialize({ name: '&tj&' }, 'name=%26tj%26');
+    serialize({ '&name&': 'tj' }, '%26name%26=tj');
+    serialize({foo:{bar: {baz: 100}}}, "foo%5Bbar%5D%5Bbaz%5D=100");
+    serialize({foo:{bar: 1, baz: 2}}, "foo%5Bbar%5D=1&foo%5Bbaz%5D=2");
   });
 });
 
@@ -37,8 +39,10 @@ describe('request.parseString()', function(){
   it('should parse', function() {
     parse('name=tj', { name: 'tj' });
     parse('name=Manny&species=cat', { name: 'Manny', species: 'cat' });
-    parse('redirect=/&ok', { redirect: '/', ok: 'undefined' });
+    parse('redirect=/&ok', { redirect: '/', ok: '' });
     parse('%26name=tj', { '&name': 'tj' });
     parse('name=tj%26', { name: 'tj&' });
+    parse("foo%5Bbar%5D%5Bbaz%5D=100", {foo:{bar: {baz: 100}}});
+    parse("foo%5Bbar%5D=1&foo%5Bbaz%5D=2", {foo:{bar: 1, baz: 2}});
   });
 });
