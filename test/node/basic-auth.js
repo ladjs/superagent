7d9321fcef841edeffd5cd30c2a25e04df9dc1a6
Kornel Lesiński <kornel@geekhood.net>
2015-12-17 23:49:53 +0000

    Prefix endpoints

--- test/node/basic-auth.js
+++ test/node/basic-auth.js
@@ -6,11 +6,11 @@ var EventEmitter = require('events').EventEmitter
   , app = express()
   , basicAuth = require('basic-auth-connect');
 
-app.get('/', basicAuth('tobi', 'learnboost'), function(req, res){
+app.get('/basic-auth', basicAuth('tobi', 'learnboost'), function(req, res){
   res.end('you win!');
 });
 
-app.get('/again', basicAuth('tobi', ''), function(req, res){
+app.get('/basic-auth/again', basicAuth('tobi', ''), function(req, res){
   res.end('you win again!');
 });
 
@@ -20,7 +20,7 @@ describe('Basic auth', function(){
   describe('when credentials are present in url', function(){
     it('should set Authorization', function(done){
       request
-      .get('http://tobi:learnboost@localhost:3010')
+      .get('http://tobi:learnboost@localhost:3010/basic-auth')
       .end(function(err, res){
         res.status.should.equal(200);
         done();
@@ -31,7 +31,7 @@ describe('Basic auth', function(){
   describe('req.auth(user, pass)', function(){
     it('should set Authorization', function(done){
       request
-      .get('http://localhost:3010')
+      .get('http://localhost:3010/basic-auth')
       .auth('tobi', 'learnboost')
       .end(function(err, res){
         res.status.should.equal(200);
@@ -43,7 +43,7 @@ describe('Basic auth', function(){
   describe('req.auth(user + ":" + pass)', function(){
     it('should set authorization', function(done){
       request
-      .get('http://localhost:3010/again')
+      .get('http://localhost:3010/basic-auth/again')
       .auth('tobi')
       .end(function(err, res){
         res.status.should.eql(200);
