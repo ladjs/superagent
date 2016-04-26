e302db1cc917ee1ebf1387001bc7156bd5f6a396
Kornel Lesiński <kornel@geekhood.net>
2016-04-26 15:59:39 +0100

    Split test

--- test/node/redirects.js
+++ test/node/redirects.js
@@ -29,43 +29,6 @@ describe('request', function(){
       });
     })
 
-    it('should retain header fields', function(done){
-      request
-      .get(base + '/header')
-      .set('X-Foo', 'bar')
-      .end(function(err, res){
-        try {
-          assert(res.body);
-          res.body.should.have.property('x-foo', 'bar');
-          done();
-        } catch(err) {
-          done(err);
-        }
-      });
-    })
-
-    it('should remove Content-* fields', function(done){
-      request
-      .post(base + '/header')
-      .type('txt')
-      .set('X-Foo', 'bar')
-      .set('X-Bar', 'baz')
-      .send('hey')
-      .end(function(err, res){
-        try {
-          assert(res.body);
-          res.body.should.have.property('x-foo', 'bar');
-          res.body.should.have.property('x-bar', 'baz');
-          res.body.should.not.have.property('content-type');
-          res.body.should.not.have.property('content-length');
-          res.body.should.not.have.property('transfer-encoding');
-          done();
-        } catch(err) {
-          done(err);
-        }
-      });
-    })
-
     it('should retain cookies', function(done){
       request
       .get(base + '/header')
@@ -74,21 +37,6 @@ describe('request', function(){
         try {
           assert(res.body);
           res.body.should.have.property('cookie', 'foo=bar;');
-          done();
-        } catch(err) {
-          done(err);
-        }
-      });
-    })
-
-    it('should preserve timeout across redirects', function(done){
-      request
-      .get(base + '/movies/random')
-      .timeout(250)
-      .end(function(err, res){
-        try {
-          assert(err instanceof Error, 'expected an error');
-          err.should.have.property('timeout', 250);
           done();
         } catch(err) {
           done(err);
@@ -255,51 +203,4 @@ describe('request', function(){
     })
   })
 
-  describe('on 303', function(){
-    it('should redirect with same method', function(done){
-      request
-      .put(base + '/redirect-303')
-      .send({msg: "hello"})
-      .redirects(1)
-      .on('redirect', function(res) {
-        res.headers.location.should.equal('/reply-method')
-      })
-      .end(function(err, res){
-        res.text.should.equal('method=get');
-        done();
-      })
-    })
-  })
-
-  describe('on 307', function(){
-    it('should redirect with same method', function(done){
-      request
-      .put(base + '/redirect-307')
-      .send({msg: "hello"})
-      .redirects(1)
-      .on('redirect', function(res) {
-        res.headers.location.should.equal('/reply-method')
-      })
-      .end(function(err, res){
-        res.text.should.equal('method=put');
-        done();
-      })
-    })
-  })
-
-  describe('on 308', function(){
-    it('should redirect with same method', function(done){
-      request
-      .put(base + '/redirect-308')
-      .send({msg: "hello"})
-      .redirects(1)
-      .on('redirect', function(res) {
-        res.headers.location.should.equal('/reply-method')
-      })
-      .end(function(err, res){
-        res.text.should.equal('method=put');
-        done();
-      })
-    })
-  })
 })
