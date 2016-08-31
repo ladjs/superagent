e50a0fbe5c84ddf2b4baeb1a4340512b96a106ef
terusus <terusus@gmail.com>
2016-08-31 18:45:38 +0200

    Added authentication with client certificates.

--- test/node/https.js
+++ test/node/https.js
@@ -7,61 +7,128 @@ var EventEmitter = require('events').EventEmitter
   , url = require('url')
   , https = require('https')
   , fs = require('fs')
+  , ca = fs.readFileSync(__dirname + '/fixtures/ca.cert.pem')
   , key = fs.readFileSync(__dirname + '/fixtures/key.pem')
   , cert = fs.readFileSync(__dirname + '/fixtures/cert.pem')
   , server;
+
+
 
 app.get('/', function(req, res){
   res.send('Safe and secure!');
 });
 
-server = https.createServer({
-  key: key,
-  cert: cert
-}, app);
+
 
 // WARNING: this .listen() boilerplate is slightly different from most tests
 // due to HTTPS. Do not copy/paste without examination.
-var base = 'https://localhost'
-before(function listen(done) {
-  server.listen(0, function listening() {
-    base += ':' + server.address().port;
-    done();
-  });
-});
+var base = 'https://localhost';
+var testEndpoint;
+
+
 
 describe('https', function(){
-
-  describe('request', function(){
-    it('should give a good response', function(done){
-      request
-      .get(base)
-      .ca(cert)
-      .end(function(err, res){
-        assert(res.ok);
-        assert('Safe and secure!' === res.text);
+  describe('certificate authority', function() {
+    before(function listen(done) {
+      server = https.createServer({
+        key: key,
+        cert: cert
+      }, app);
+      server.listen(0, function listening() {
+        testEndpoint = base + ':' + server.address().port;
         done();
-      });
-    });
-  });
+      })
+    })
 
-  describe('.agent', function () {
-    it('should be able to make multiple requests without redefining the certificate', function(done){
-      var agent = request.agent({ca: cert});
-      agent
-      .get(base)
-      .end(function(err, res){
-        assert(res.ok);
-        assert('Safe and secure!' === res.text);
-        agent
-        .get(url.parse(base))
+    after(function() {
+      server.close()
+    })
+
+    describe('request', function(){
+      it('should give a good response', function(done){
+        request
+        .get(testEndpoint)
+        .ca(ca)
         .end(function(err, res){
           assert(res.ok);
           assert('Safe and secure!' === res.text);
           done();
-        });
-      });
-    });
-  });
+        })
+      })
+    })
 
+    describe('.agent', function () {
+      it('should be able to make multiple requests without redefining the certificate', function(done){
+        var agent = request.agent({ca: ca});
+        agent
+        .get(testEndpoint)
+        .end(function(err, res){
+          assert(res.ok);
+          assert('Safe and secure!' === res.text);
+          agent
+          .get(url.parse(testEndpoint))
+          .end(function(err, res){
+            assert(res.ok);
+            assert('Safe and secure!' === res.text);
+            done();
+          })
+        })
+      })
+    })
+  })
+
+
+  describe('client certificates', function() {
+    before(function listen(done) {
+      server = https.createServer({
+        ca: ca,
+        key: key,
+        cert: cert,
+        requestCert: true,
+        rejectUnauthorized: true
+      }, app);
+      server.listen(0, function listening() {
+        testEndpoint = base + ':' + server.address().port;
+        done();
+      })
+    })
+
+    after(function() {
+      server.close()
+    })
+
+    describe('request', function(){
+      it('should give a good response with client certificates and CA', function(done){
+        request
+        .get(testEndpoint)
+        .ca(ca)
+        .key(key)
+        .cert(cert)
+        .end(function(err, res){
+          assert(res.ok);
+          assert('Safe and secure!' === res.text);
+          done();
+        })
+      })
+    })
+
+    describe('.agent', function () {
+      it('should be able to make multiple requests without redefining the certificates', function(done){
+        var agent = request.agent({ca, key, cert});
+        agent
+        .get(testEndpoint)
+        .end(function(err, res){
+          assert(res.ok);
+          assert('Safe and secure!' === res.text);
+          agent
+          .get(url.parse(testEndpoint))
+          .end(function(err, res){
+            assert(res.ok);
+            assert('Safe and secure!' === res.text);
+            done();
+          })
+        })
+      })
+    })
+  })
 })
