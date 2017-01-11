7c807014815ebd7c9d42c176350c3e19bbd5242d
Alexander Pope <alex@pope-industries.com>
2017-01-11 13:21:26 +0100

    added Node retry handling for timeouts

--- test/retry.js
+++ test/retry.js
@@ -30,11 +30,32 @@ describe('.retry(count)', function(){
     });
   });
 
+  it('should handle server timeout error after repeat attempt', function(done) {
+    request
+    .get(base + '/delay/150')
+    .timeout(50)
+    .retry(2)
+    .end(function(err, res){
+      assert(err, 'expected an error');
+      assert.equal(3, err.attempts, 'expected an error with .attempts');
+      assert.equal('number', typeof err.timeout, 'expected an error with .timeout');
+      assert.equal('ECONNABORTED', err.code, 'expected abort error code')
+      done();
+    });
+  });
+
+  it('should handle successful request after repeat attempt from server timeout', function(done) {
+    request
+    .get(base + '/delay/150/ok')
+    .timeout(50)
+    .retry(2)
+    .end(function(err, res){
+      assert(res instanceof request.Response, 'respond with Response');
+      assert(res.ok, 'response should be ok');
+      assert(res.text, 'res.text');
+      done();
+    });
+  });
+
   it('should handle successful redirected request after repeat attempt from server error');
-
-  it('should handle server timeout error after repeat attempt');
-
-  it('should handle successful request after repeat attempt from server timeout');
-
-  it('should emit event on retry');
 })
