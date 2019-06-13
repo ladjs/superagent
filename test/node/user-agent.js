9c117a606bd5f7b44cee2ad339ab57f187fb9bf2
Apoorv Mishra <apoorv@mymandir.com>
2019-06-13 19:04:56 +0530

    fix(#1479): disable setting default user-agent
    Revert "Disable setting default user-agent"
    
    This reverts commit 01841cc6fc2e5884b2259bf369d0ffba8d5618c1.
    
    Disable setting default user-agent
    
    Remove unused var

--- test/node/user-agent.js
+++ test/node/user-agent.js
@@ -6,32 +6,9 @@ const setup = require('../support/setup');
 const base = setup.uri;
 
 describe('req.get()', () => {
-  it('should set a default user-agent', () =>
+  it('should not set a default user-agent', () =>
     request.get(`${base}/ua`).then(res => {
       assert(res.headers);
-      assert(res.headers['user-agent']);
-      assert(
-        /^node-superagent\/\d+\.\d+\.\d+(?:-[a-z]+\.\d+|$)/.test(
-          res.headers['user-agent']
-        )
-      );
+      assert(!res.headers['user-agent']);
     }));
-
-  it('should be able to override user-agent', () =>
-    request
-      .get(`${base}/ua`)
-      .set('User-Agent', 'foo/bar')
-      .then(res => {
-        assert(res.headers);
-        assert.equal(res.headers['user-agent'], 'foo/bar');
-      }));
-
-  it('should be able to wipe user-agent', () =>
-    request
-      .get(`${base}/ua`)
-      .unset('User-Agent')
-      .then(res => {
-        assert(res.headers);
-        assert.equal(res.headers['user-agent'], void 0);
-      }));
 });
