e196345074f57987c166283c302d06d661744f14
Robert Nagy <ronagy@icloud.com>
2022-01-18 22:01:01 +0100

    lazy access body (#1634)
    Refs: https://github.com/nodejs/node/pull/39520#issuecomment-889697136

--- src/node/response.js
+++ src/node/response.js
@@ -34,7 +34,6 @@ function Response(request) {
   this.request = request;
   this.req = request.req;
   this.text = res.text;
-  this.body = res.body === undefined ? {} : res.body;
   this.files = res.files || {};
   this.buffered = request._resBuffered;
   this.headers = res.headers;
@@ -47,6 +46,21 @@ function Response(request) {
   res.on('close', this.emit.bind(this, 'close'));
   res.on('error', this.emit.bind(this, 'error'));
 }
+
+// Lazy access res.body.
+// https://github.com/nodejs/node/pull/39520#issuecomment-889697136
+Object.defineProperty(Response.prototype, 'body', {
+  get () {
+    return this._body !== undefined
+      ? this._body
+      : this.res.body !== undefined
+        ? this.res.body
+        : {};
+  },
+  set (value) {
+    this._body = value;
+  }
+});
 
 /**
  * Inherit from `Stream`.
